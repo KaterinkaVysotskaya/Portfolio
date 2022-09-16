5764a7d546b15db0fef7170d42360e283e37f7e8
KaterinkaVysotskaya <katerinka.vysotskaya@gmail.com>
2022-09-16 21:45:06 +0300

    layout

---
+++ src/common/components/title/Title.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import s from './Title.module.scss'
+
+function Title(props) {
+  return (
+            <div className={s.title}>
+                <h2 >{props.text}</h2>
+            </div>
+
+  );
+}
+
+export default Title;
